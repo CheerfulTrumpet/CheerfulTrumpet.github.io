b7e7ebb5d7bb24d20ac92045c2777259efd98f9d
CheerfulTrumpet <gotmail1947@gmail.com>
2025-12-06 16:56:00 -0500

    Add particle network animation to canvas

--- script.js
+++ script.js
@@ -80,3 +80,115 @@
           // Run immediately and then every second
           updateTimeAndGreeting();
           setInterval(updateTimeAndGreeting, 1000);
+
+/* ------------------------------------------------
+   PARTICLE NETWORK ANIMATION
+   ------------------------------------------------ */
+const canvas = document.getElementById("background-canvas");
+const ctx = canvas.getContext("2d");
+
+// Resize canvas to fill screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+// Create Particle Class (Object Oriented Programming!)
+class Particle {
+    constructor() {
+        // Random position
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        // Random velocity (movement speed)
+        // We subtract 0.5 to allow negative movement (left/up)
+        this.directionX = (Math.random() * 1) - 0.5;
+        this.directionY = (Math.random() * 1) - 0.5;
+        // Size and Color
+        this.size = (Math.random() * 2) + 1;
+        this.color = '#45a29e'; // A subtle teal color
+    }
+
+    // Method to draw individual particle
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    // Method to update particle position
+    update() {
+        // Check if particle hits the edge of screen, if so, reverse direction
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        // Move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+
+        // Draw particle
+        this.draw();
+    }
+}
+
+// Create particle array
+function init() {
+    particlesArray = [];
+    // Create density based on screen size (prevent overcrowding on mobile)
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+    // Clear the canvas on every frame so particles don't leave trails
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+    }
+    connect(); // Draw the lines
+}
+
+// Check if particles are close enough to draw a line (Graph Theory!)
+function connect() {
+    let opacityValue = 1;
+    for (let a = 0; a < particlesArray.length; a++) {
+        for (let b = a; b < particlesArray.length; b++) {
+            // Pythagorean theorem to find distance
+            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
+                         + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
+            
+            // If close enough (distance < 140*140)
+            if (distance < (canvas.width/7) * (canvas.height/7)) {
+                opacityValue = 1 - (distance/20000);
+                ctx.strokeStyle = 'rgba(69, 162, 158,' + opacityValue + ')';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+// Handle Window Resize (so particles don't stretch)
+window.addEventListener('resize', 
+    function() {
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        init();
+    }
+);
+
+// Start the animation
+init();
+animate();
